28d2476692fa08ba3c78eb71b1dfea9a938097ad
Mulang Su <sumulang@gmail.com>
2020-12-27 14:30:41 +0800

    added world name in whereis command

--- src/main/java/io/github/sweeper777/pluginplayground/PluginPlayground.kt
+++ src/main/java/io/github/sweeper777/pluginplayground/PluginPlayground.kt
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class PluginPlayground : JavaPlugin() {
     override fun onEnable() {
-        getCommand("whereis")?.setExecutor { sender, cmd, _, args ->
+        getCommand("whereis")?.setExecutor { sender, _, _, args ->
             val playerToBeLocated = args.firstOrNull()?.let {
                 server.getPlayer(it)
             } ?: sender as? Player
@@ -14,7 +14,10 @@ class PluginPlayground : JavaPlugin() {
                 val message = "${playerToBeLocated.displayName} is at " +
                         "x=${playerToBeLocated.location.blockX}, " +
                         "y=${playerToBeLocated.location.blockY}, " +
-                        "z=${playerToBeLocated.location.blockZ}"
+                        "z=${playerToBeLocated.location.blockZ}" +
+                        (playerToBeLocated.location.world?.name?.let { worldName ->
+                            ", in '${worldName}'"
+                        } ?: "")
                 sender.sendMessage(message)
             } else {
                 sender.sendMessage("Invalid Player Specified")
