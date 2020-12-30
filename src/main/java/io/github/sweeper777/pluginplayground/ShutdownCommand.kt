b9c017e5188d21e1e82519098ee1f93b969ece99
Mulang Su <sumulang@gmail.com>
2020-12-30 16:32:44 +0800

    added CountDownTask

--- src/main/java/io/github/sweeper777/pluginplayground/ShutdownCommand.kt
+++ src/main/java/io/github/sweeper777/pluginplayground/ShutdownCommand.kt
@@ -12,3 +12,21 @@ class ShutdownCommand(private val plugin: JavaPlugin): CommandExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
 }
 
+class CountDownTask(val start: Long, private val server: Server): Runnable {
+    var current: Long = start
+
+    override fun run() {
+        if (current < 0L) {
+            return
+        }
+        if (current == 0L) {
+            server.dispatchCommand(server.consoleSender, "stop")
+            current--
+            return
+        }
+        if (current == start || current == 60L || current == 30L || current <= 10L) {
+            server.broadcastMessage("Server shutting down in $current seconds... Please be prepared!")
+        }
+        current--
+    }
+}
