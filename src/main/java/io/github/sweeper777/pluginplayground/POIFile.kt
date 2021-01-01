91f7a875e4ad4f2b658e65b14ae1f1b466315b66
Mulang Su <sumulang@gmail.com>
2021-01-01 10:53:13 +0800

    Create POIFile.kt

---
+++ src/main/java/io/github/sweeper777/pluginplayground/POIFile.kt
@@ -0,0 +1,19 @@
+package io.github.sweeper777.pluginplayground
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.UUID
+
+
+class POIFile(private val path: String) {
+    private val poisByUUID: MutableMap<String, MutableList<POI>>
+
+    init {
+        val reader = InputStreamReader(FileInputStream(path))
+        val typeToken = object : TypeToken<HashMap<String, ArrayList<POI>>>(){}
+        poisByUUID = Gson().fromJson(reader, typeToken.type)
+    }
+
+}
