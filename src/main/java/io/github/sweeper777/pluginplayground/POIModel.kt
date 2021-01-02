03911e1b06386e3fad4e7d52b011747646771387
Mulang Su <sumulang@gmail.com>
2021-01-02 10:49:48 +0800

    added a secondary constructor to POILocation

--- src/main/java/io/github/sweeper777/pluginplayground/POIModel.kt
+++ src/main/java/io/github/sweeper777/pluginplayground/POIModel.kt
@@ -1,4 +1,10 @@
 package io.github.sweeper777.pluginplayground
 
-data class POILocation(val x: Int, val y: Int, val z: Int, val world: String)
+import org.bukkit.Location
+
+data class POILocation(val x: Int, val y: Int, val z: Int, val world: String) {
+    constructor(location: Location) : this(
+        location.blockX, location.blockY, location.blockZ, location.world?.name ?: "unknown"
+    )
+}
 data class POI(val name: String, val location: POILocation)
