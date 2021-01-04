dae5cd4ef1c3e81433ed71a4f06139bf899d2e72
Mulang Su <sumulang@gmail.com>
2021-01-04 10:21:02 +0800

    added initialiseItems

--- src/main/java/io/github/sweeper777/pluginplayground/MyGUI.kt
+++ src/main/java/io/github/sweeper777/pluginplayground/MyGUI.kt
@@ -21,6 +21,29 @@ import org.bukkit.inventory.Inventory
 class MyGUI : Listener {
     private val inv: Inventory = Bukkit.createInventory(null, 9, "My GUI")
 
+    init {
+        initializeItems()
+    }
+
+    private fun initializeItems() {
+        inv.addItem(
+            createGuiItem(
+                Material.DIAMOND,
+                "Item 1",
+                "§aFirst line of the lore",
+                "§bSecond line of the lore"
+            )
+        )
+        inv.addItem(
+            createGuiItem(
+                Material.IRON_INGOT,
+                "§bItem 2",
+                "§aFirst line of the lore",
+                "§bSecond line of the lore"
+            )
+        )
+    }
+
     private fun createGuiItem(material: Material, name: String, vararg lore: String): ItemStack {
         val item = ItemStack(material, 1)
         val meta = item.itemMeta
